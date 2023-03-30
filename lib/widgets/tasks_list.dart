94375ac334a5b1aeb0c21c6932b2afd4ae1e22b2
Do Cao Minh <90237044+imminh20x@users.noreply.github.com>
2023-03-30 12:45:18 +0700

    update task list
    modify code

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -29,32 +29,3 @@ class TasksList extends StatelessWidget {
     );
   }
 }
-
-// class TasksList extends StatefulWidget {
-//   final List<Task> tasks;
-//   TasksList(this.tasks);
-
-//   @override
-//   State<TasksList> createState() => _TasksListState();
-// }
-
-// class _TasksListState extends State<TasksList> {
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemBuilder: (context, index) {
-//         return TaskTile(
-//             taskTitle: widget.tasks[index].name,
-//             isChecked: widget.tasks[index].isDone!,
-//             checkboxCallbacks: (bool? checkboxState) {
-//               setState(() {
-//                 //isChecked = checkboxState
-//                 widget.tasks[index].toggleDone();
-//               });
-//             });
-//       },
-//       itemCount: widget.tasks.length,
-//     );
-//   }
-// }
